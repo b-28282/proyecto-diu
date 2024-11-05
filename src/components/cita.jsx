b6d497af061a06ffac928dbf7dcae3488019ea99
b-28282 <benjamin.saez@sansano.usm.cl>
2024-11-05 12:08:43 -0300

    Estilos para cita

--- src/components/cita.jsx
+++ src/components/cita.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export const Cita = ({ nombre, fecha, descripcion }) => {
   return (
-    <>
+    <div className="cita">
       <h3>{nombre}</h3>
       <p>{fecha}</p>
       <p>{descripcion}</p>
-    </>
+    </div>
   );
 }
