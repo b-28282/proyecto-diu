9791ceb89f388892a47cfa8575fe6e213ce5c2c3
AntonioReyH <106408250+AntonioReyH@users.noreply.github.com>
2024-11-05 20:51:30 -0300

    Formulario final

--- src/pages/agendar_rec_recurrente.jsx
+++ src/pages/agendar_rec_recurrente.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
-import WeeklyReminderForm from '../components/recordatorio-continuo';
 import { useNavigate } from 'react-router-dom';
+import Form1 from '../components/form1';
+
 
 export const AgendarRecRecurrentePage = () => {
-  const navigate = useNavigate();
-  const handleNavigate = () => {
-    navigate('/agendar_rec'); 
-  };
-  const handleSubmit = (formData) => {
-    console.log("Datos del formulario:", formData);
-    navigate('/citas'); 
-  };
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+    navigate('/agendar_rec'); // Cambia a la ruta que deseas
+    };
+    const handleNavigate1 = () => {
+    navigate('/recordatorios'); // Cambia a la ruta que deseas
+    };
   return (
     <>
       <h1>Recordatorio Semanal</h1>
-      <WeeklyReminderForm onSubmit={handleSubmit} />
 
+      <Form1 onSubmit={handleNavigate1} />
       <button onClick={handleNavigate}>
-        Atrás
+      Atrás
       </button>
     </>
-
   )
 }
 
